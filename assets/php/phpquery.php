b7a51bf99e2f60bbf8d27f30c4d53c8930eb62f9
gu0p <k168003@kccollege.ac.jp>
2017-11-15 15:51:44 +0900

    aaaaaaaa

--- assets/php/phpquery.php
+++ assets/php/phpquery.php
@@ -1,14 +1,30 @@
 <?php
 require_once("phpQuery-onefile.php");
 $doc = phpQuery::newDocumentFile("http://www.jma.go.jp/jp/amedas_h/today-46106.html?areaCode=&groupCode=");
+
+$i = 1;
+$cnt = 0;
 foreach($doc["table:eq(5) tr"] as $row)
 {
     //Še—v‘fŽæ“¾
-    $key   = pq($row)->find("td:eq(0)")->text();
-    $aaa   = pq($row)->find("td:eq(1)")->text();
+    $key[$i] = pq($row)->find("td:eq(0)")->text();
+    $aaa[$i] = pq($row)->find("td:eq(1)")->text();
 
-    //•\Ž¦
-    echo $key . " ";
-    echo $aaa . " ";
-	
+	$i++;
+	$cnt++;
 }
+
+?>
+
+
+
+
+
+<table border="1">
+	<?php foreach(range(1, $cnt) as $i): ?>
+		<tr>
+			<td><?=$key[$i]?></td>
+			<td><?=$aaa[$i]?></td>
+		</tr>
+	<?php endforeach; ?>
+</table>
