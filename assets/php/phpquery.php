41233ff75e4f30943c3c97df0575e4c9ae8fb0e5
gu0p <k168003@kccollege.ac.jp>
2017-11-15 15:58:29 +0900

    アメダス（表形式）作成

--- assets/php/phpquery.php
+++ assets/php/phpquery.php
@@ -7,8 +7,15 @@ $cnt = 0;
 foreach($doc["table:eq(5) tr"] as $row)
 {
     //Še—v‘fŽæ“¾
-    $key[$i] = pq($row)->find("td:eq(0)")->text();
-    $aaa[$i] = pq($row)->find("td:eq(1)")->text();
+    $time[$i] = pq($row)->find("td:eq(0)")->text();
+    $temp[$i] = pq($row)->find("td:eq(1)")->text();
+    $prec[$i] = pq($row)->find("td:eq(2)")->text();
+    $widi[$i] = pq($row)->find("td:eq(3)")->text();
+    $wisp[$i] = pq($row)->find("td:eq(4)")->text();
+    $suns[$i] = pq($row)->find("td:eq(5)")->text();
+    $snow[$i] = pq($row)->find("td:eq(6)")->text();
+    $humi[$i] = pq($row)->find("td:eq(7)")->text();
+    $atmo[$i] = pq($row)->find("td:eq(8)")->text();
 
 	$i++;
 	$cnt++;
@@ -23,8 +30,15 @@ foreach($doc["table:eq(5) tr"] as $row)
 <table border="1">
 	<?php foreach(range(1, $cnt) as $i): ?>
 		<tr>
-			<td><?=$key[$i]?></td>
-			<td><?=$aaa[$i]?></td>
+			<td><?=$time[$i]?></td>
+			<td><?=$temp[$i]?></td>
+			<td><?=$prec[$i]?></td>
+			<td><?=$widi[$i]?></td>
+			<td><?=$wisp[$i]?></td>
+			<td><?=$suns[$i]?></td>
+			<td><?=$snow[$i]?></td>
+			<td><?=$humi[$i]?></td>
+			<td><?=$atmo[$i]?></td>
 		</tr>
 	<?php endforeach; ?>
 </table>
